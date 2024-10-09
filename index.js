8627067536aa48be2a9efa26d8dacadab58edca3
S4N14G0 <sn8560697@gmail.com>
2024-10-09 18:04:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.create({
+    trigger: ".PantallaPrincipal",
+    start: "top top", // Cuando la parte superior de la sección fija llega a la parte superior de la ventana
+    end: "top", // Ajusta esto según la longitud de tu contenido
+    pin: true, // Fija la sección
+    markers: false, // Activa los marcadores para ver el inicio y fin
+});
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
