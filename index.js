0a8b9a2fe32676df45dc8a283ca431c9ed9026ad
S4N14G0 <sn8560697@gmail.com>
2024-10-29 17:35:51 -0300

    modified:   index.html 	modified:   index.js 	new file:   png-logos/15s Corona.webp 	new file:   png-logos/Anillos.png 	new file:   png-logos/Fiestas.png 	new file:   png-logos/diploma.png     modified:   style.css

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+function smoothScroll(event, targetId) {
+    event.preventDefault();
+    const target = document.getElementById(targetId);
+    target.scrollIntoView({ behavior: 'smooth' });
+}
